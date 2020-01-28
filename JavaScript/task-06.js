03f04e37d5c5b459bac59a7c0a4eb312d944a1ec
Anastasiia Sereda <nastiasereda.1994@gmail.com>
2020-01-28 20:15:21 +0200

    Added new version

--- JavaScript/task-06.js
+++ JavaScript/task-06.js
@@ -1,14 +1,33 @@
+// const input = document.querySelector('#validation-input');
+
+// input.addEventListener('blur', lengthCheck);
+
+// function lengthCheck() {
+
+//     if (input.value.length != input.dataset.length) {
+//         input.classList.add('invalid')
+//     } else if (input.classList.contains('invalid')) {
+//         input.classList.remove('invalid');
+//     }
+
+//     input.classList.add('valid')
+// }
+
+
+
 const input = document.querySelector('#validation-input');
 
-input.addEventListener('blur', lengthCheck);
+input.addEventListener('blur', function() {
+  insertedText(input.value.length);
+});
 
-function lengthCheck() {
+const insertedText = function (value) {
+  const number = parseInt(input.dataset.length);
 
-    if (input.value.length != input.dataset.length) {
-        input.classList.add('invalid')
-    } else if (input.classList.contains('invalid')) {
-        input.classList.remove('invalid');
-    }
-
-    input.classList.add('valid')
-}
+  if (value === number) {
+    input.classList.add("valid");
+  } else {
+    input.classList.remove("valid");
+    input.classList.add("invalid");
+  }
+};
