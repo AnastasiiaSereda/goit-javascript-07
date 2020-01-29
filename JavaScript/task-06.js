338210da718e7e8fa06cd5183e047777c6073efd
Anastasiia Sereda <nastiasereda.1994@gmail.com>
2020-01-29 19:24:16 +0200

    Added second version

--- JavaScript/task-06.js
+++ JavaScript/task-06.js
@@ -1,33 +1,33 @@
-// const input = document.querySelector('#validation-input');
-
-// input.addEventListener('blur', lengthCheck);
-
-// function lengthCheck() {
-
-//     if (input.value.length != input.dataset.length) {
-//         input.classList.add('invalid')
-//     } else if (input.classList.contains('invalid')) {
-//         input.classList.remove('invalid');
-//     }
-
-//     input.classList.add('valid')
-// }
-
-
-
 const input = document.querySelector('#validation-input');
 
-input.addEventListener('blur', function() {
-  insertedText(input.value.length);
-});
+input.addEventListener('blur', lengthCheck);
 
-const insertedText = function (value) {
-  const number = parseInt(input.dataset.length);
+function lengthCheck() {
 
-  if (value === number) {
-    input.classList.add("valid");
-  } else {
-    input.classList.remove("valid");
-    input.classList.add("invalid");
-  }
-};
+    if (input.value.length != input.dataset.length) {
+        input.classList.add('invalid')
+    } else if (input.classList.contains('invalid')) {
+        input.classList.remove('invalid');
+    }
+
+    input.classList.add('valid')
+}
+
+
+
+// const input = document.querySelector('#validation-input');
+
+// input.addEventListener('blur', function() {
+//   insertedText(input.value.length);
+// });
+
+// const insertedText = function (value) {
+//   const number = parseInt(input.dataset.length);
+
+//   if (value === number) {
+//     input.classList.add("valid");
+//   } else {
+//     input.classList.remove("valid");
+//     input.classList.add("invalid");
+//   }
+// };
